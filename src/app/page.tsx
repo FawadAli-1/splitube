58ef10bc70668a3cae86d79195e56358d5ca9ff4
Fawad Ali <becomebetter101@gmail.com>
2024-11-18 14:50:56 +0500

    Added privacy policy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
       <section>
         <SignedIn>
           <HeroSection />
+          <Footer/>
         </SignedIn>
         <SignedOut>
           <LandingPage />
